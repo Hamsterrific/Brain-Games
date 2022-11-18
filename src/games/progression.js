c142a6a9158e80288e53dc6c9c90a2fca13f06e3
Hamsterrific <simbul@gmail.com>
2022-11-18 23:00:02 +0400

    refactor

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,37 +2,30 @@ import run from '../index.js';
 import { generateNumber, generateIndex } from '../utils.js';
 
 const gameTask = 'What number is missing in the progression?';
-const minNumber = 1;
-const maxNumber = 100;
+const minStart = 1;
+const maxStart = 100;
 const minStep = 1;
 const maxStep = 5;
 const minLength = 5;
 const maxLength = 10;
 
-const makeProgression = (number, step, length) => {
+const makeProgression = (start, step, length) => {
   const progression = [];
   for (let i = 0; i < length; i += 1) {
-    progression.push(number + (step * i));
+    progression.push(start + step * i);
   }
   return progression;
 };
 
-const hideNumber = (numbers) => {
-  const index = generateIndex(numbers);
-  const hiddenNumber = numbers[index];
-  const result = numbers;
-  result[index] = '..';
-  return [result, hiddenNumber];
-};
-
 const generateRound = () => {
-  const start = generateNumber(minNumber, maxNumber);
+  const start = generateNumber(minStart, maxStart);
   const step = generateNumber(minStep, maxStep);
   const length = generateNumber(minLength, maxLength);
   const progression = makeProgression(start, step, length);
-  const [hiddenProgression, hiddenNumber] = hideNumber(progression);
-  const question = hiddenProgression.join(' ');
-  const correctAnswer = String(hiddenNumber);
+  const hiddenNumber = generateIndex(progression);
+  const correctAnswer = String(progression[hiddenNumber]);
+  progression[hiddenNumber] = '..';
+  const question = progression.join(' ');
   return [question, correctAnswer];
 };
 
